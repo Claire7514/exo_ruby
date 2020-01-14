4433c618d0d50381cea216e60f9d0dffbd838b81
Claire Dubas <dub.claire@icloud.com>
2020-01-14 16:20:44 +0100

    commentaires

--- exo_05.rb
+++ exo_05.rb
@@ -1,22 +1,22 @@
-puts "On va compter le nombre d'heures de travail à THP"
-puts "Travail : #{10 * 5 * 11}"
-puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+puts "On va compter le nombre d'heures de travail à THP" #affiche chaîne de caractères
+puts "Travail : #{10 * 5 * 11}" #affichage string et calcul
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #idem
 
-puts "Et en secondes ?"
+puts "Et en secondes ?" #affichage
 
-puts 10 * 5 * 11 * 60 * 60
+puts 10 * 5 * 11 * 60 * 60 #calcul
 
-puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #affichage
 
-puts 3 + 2 < 5 - 7
+puts 3 + 2 < 5 - 7 #true or false
 
-puts "Ça fait combien 3 + 2 ? #{3 + 2}"
-puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #affichage et calcul
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #idem
 
-puts "Ok, c'est faux alors !"
+puts "Ok, c'est faux alors !"# affichage
 
-puts "C'est drôle ça, faisons-en plus :"
+puts "C'est drôle ça, faisons-en plus :"#affichage
 
-puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
-puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
-puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #affichage et true or false
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #idem
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #idem
--- exo_07_c.rb
+++ exo_07_c.rb
@@ -1,3 +1,7 @@
 user_name = gets.chomp
 puts user_name
 
+#différence entre les 3 programmes : la mise en forme qui change.
+#le premier affiche une question 
+#le deuxième affiche une quastion et indique à l'utilisateur où il faut taper
+#le troisième n'indique rien
